75d1b29568e9b97223744dec3efc09550021d056
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-05-25 03:08:39 -0400

    More styling and delete function

--- popup.js
+++ popup.js
@@ -6,9 +6,12 @@ window.addEventListener("DOMContentLoaded", function () {
     allTabs = res;
     res.forEach((tab) => {
       const tabs = document.querySelector('#tabs');
-      console.log(tabs);
+      const div = document.createElement('div');
+      div.classList.add('tab')
       const tabEl = document.createElement('li');
       tabEl.innerText = tab.title;
+      const deleteButton = document.createElement('span');
+      deleteButton.innerHTML = '<i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg></i>';
       if (tab.active) {
         tabEl.classList.add('active');
       }
@@ -16,10 +19,14 @@ window.addEventListener("DOMContentLoaded", function () {
         chrome.tabs.update(tab.id, { active: true });
         window.close();
       });
-      tabEl.addEventListener('dblclick', function () {
+      deleteButton.addEventListener('click', function (e) {
+        this.classList.add('delete');
         chrome.tabs.remove(tab.id);
+        this.parentElement.remove()
       })
-      tabs.appendChild(tabEl)
+      div.appendChild(deleteButton);
+      div.appendChild(tabEl)
+      tabs.appendChild(div);
     });
   });
 });
