8091805150b986e55086f4f15517633e4a6e8dcb
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-05-25 01:01:52 -0400

    Tab list and styles

--- popup.js
+++ popup.js
@@ -1,15 +1,28 @@
 console.log('popup.js')
 window.addEventListener("DOMContentLoaded", function () {
   console.log('DOM loaded');
-  const tabs = document.querySelector('#tabs');
-  const allTabs = [];
+  let allTabs = [];
   chrome.runtime.sendMessage({ type: "getTabs" }, function (res) {
-    console.log(res)
-    // res.forEach(el => {
-    //   console.log(el.Tab)
-    // })
+    allTabs = res;
+    res.forEach((tab) => {
+      const tabs = document.querySelector('#tabs');
+      console.log(tabs);
+      const tabEl = document.createElement('li');
+      tabEl.innerText = tab.title;
+      if (tab.active) {
+        tabEl.classList.add('active');
+      }
+      tabEl.addEventListener('click', function () {
+        chrome.tabs.update(tab.id, { active: true });
+        window.close();
+      });
+      tabEl.addEventListener('dblclick', function () {
+        chrome.tabs.remove(tab.id);
+      })
+      tabs.appendChild(tabEl)
+    });
   });
-  chrome.runtime.sendMessage({ type: "getTabs" }, function (res) {
-    console.log(res)
-  })
 });
+  // chrome.runtime.sendMessage({ type: "getTabs" }, function (res) {
+  //   console.log(res)
+  // })
