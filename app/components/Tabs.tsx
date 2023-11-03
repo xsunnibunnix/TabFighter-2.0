0aa86b5d983fc3455d597e0faf53cfd5804fd323
ianmacdougald <35744006+ianmacdougald@users.noreply.github.com>
2023-11-03 11:41:51 -0400

    Implement selection functionality in Tabs

--- app/components/Tabs.tsx
+++ app/components/Tabs.tsx
@@ -6,8 +6,8 @@ import { TabContext } from '../context/TabContext';
 import getTabs from '../utils/getTabs';
 import { RemoveContext } from '../context/RemoveContext';
 import { SoundContext } from '../context/SoundContext';
+import { SelectContext } from '../context/SelectContext';
 import { SelectButton } from './SelectButton';
-import { SelectContext, tabs } from '../context/SelectContext';
 
 interface TabsProps {
   tabId: number | undefined,
@@ -25,15 +25,14 @@ export interface LiProps extends React.LiHTMLAttributes<HTMLLIElement> {
 const Tabs = ({ tabId, active, title, height, width, ...LiProps }: TabsProps) => {
   const [yoshi, setYoshi] = useState<boolean>(false);
   const [hadouken, setHadouken] = useState<boolean>(false);
-  const [checked, setChecked] = useState<boolean>(false);
 
+  const selectedTabs = useContext(SelectContext)?.selectedTabs; 
+  const addToSelectedTabs = useContext(SelectContext)?.addToSelectedTabs; 
+  const removeFromSelectedTabs = useContext(SelectContext)?.removeFromSelectedTabs;
+  
   const setAllTabs = useContext(TabContext)?.setAllTabs;
   const tabToDelete = useContext(RemoveContext)?.tabToDelete;
   const soundOn = useContext(SoundContext)?.soundOn;
-
-  const selectedTabs = useContext(SelectContext)?.selectedTabs;
-  const addToSelectedTabs = useContext(SelectContext)?.addToSelectedTabs; 
-  const removeFromSelectedTabs = useContext(SelectContext)?.removeFromSelectedTabs;
 
   const goToTab = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
     const target = e.target as HTMLLIElement;
@@ -51,48 +50,42 @@ const Tabs = ({ tabId, active, title, height, width, ...LiProps }: TabsProps) =>
     setTimeout(chrome.windows.update, timeoutTime, windowId, { focused: true, state: 'normal', height, width });
   };
 
-  const removeTabs = (...inputs: Array<number>) => { 
-    setTimeout(() => { 
-      inputs.forEach(item => { 
-        try { 
-        if (item) chrome.tabs.remove(item)
-        } catch (e) { 
-          console.warn("Failed to remove tab with " + item + " id");
-        }
-      });
-    }, soundOn ? (active ? 1500 : 1250) : 300);
+  const removeTabs = (...tabs: Array<number>) => { 
+    setTimeout(
+      () => tabs.forEach(tab => chrome.tabs.remove(tab)), 
+      soundOn ? (active ? 1500 : 1250) : 300
+    );
   }
 
-  // Refactored to support closing all checked tabs if the current tab is also checked
+  const checked = (tabId && selectedTabs) ? selectedTabs.has(tabId) : false;
+
   const closeTab = () => {
-    if (soundOn) { 
-      setHadouken(true); 
+    if (soundOn) {
+      setHadouken(true);
       setTimeout(() => setHadouken(prev => !prev), 2000);
     }
-
-    if (tabId) { 
-      if (!checked) removeTabs(tabId);
-      else if (selectedTabs) removeTabs(...selectedTabs);
-    }
+    
+    if (checked && selectedTabs) removeTabs(...selectedTabs)
+    else if (tabId) removeTabs(tabId);
 
     setTimeout(() => getTabs().then(tabs => {
+      if (removeFromSelectedTabs) removeFromSelectedTabs();
       if (setAllTabs) setAllTabs(tabs);
-      setChecked(false);
     }), soundOn ? 1300 : 400);
   };
 
   const selectTab = () => { 
-    if (removeFromSelectedTabs && addToSelectedTabs) { 
-      if (!checked) addToSelectedTabs(tabId)
-      else removeFromSelectedTabs(tabId); 
+    if (selectedTabs && addToSelectedTabs && removeFromSelectedTabs && tabId) { 
+      if (!selectedTabs.has(tabId)) addToSelectedTabs(tabId)
+      else removeFromSelectedTabs(tabId)
     }
   }
-
+  
   return (
     <div className={`tab-li flex w-full ${tabToDelete === tabId ? 'delete' : ''} ${checked ? 'is-selected' : ''}`} id={String(tabId)} >
       <DeleteButton closeTab={closeTab} tabId={String(tabId)} />
       {hadouken && soundOn && <Hadouken play={hadouken} />}
-      <SelectButton selectTab={selectTab} checked={checked} tabId={String(tabId)}/>
+      <SelectButton selectTab={selectTab} tabId={String(tabId)} checked={checked}/>
       <li className={`flex items-center list-none w-full cursor-pointer p-1 pl-2.5 ${active ? 'active' : ''}`} onClick={(e) => goToTab(e)} {...LiProps}>{title}</li>
       {yoshi && soundOn && <Yoshi play={yoshi} />}
     </div>
