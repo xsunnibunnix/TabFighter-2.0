8a4dbec57f25ad6249e9b30887be6d9c0d4448eb
Sunny Pacheco <pacheco.sunny@gmail.com>
2024-03-12 21:31:37 -0400

    Add trash button on tab select

--- app/components/Toolbar/ToolbarLeft.tsx
+++ app/components/Toolbar/ToolbarLeft.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { Fatality } from '../Sounds/Fatality';
 import { useTabContext } from '../../context/TabContext';
 import { useSoundContext } from '../../context/SoundContext';
@@ -6,24 +8,21 @@ import { useThemeContext } from "../../context/ThemeContext";
 import { useSelectContext } from '../../context/SelectContext';
 import { Tab } from '../../../types';
 import getTabs from '../../utils/getTabs';
-import darkRandom from '../../icons/darkrandom.svg';
-import lightRandom from '../../icons/lightrandom.svg';
+import random from '../../icons/random.svg';
+import trash from '../../icons/trash.svg';
 
 const ToolbarLeft = () => {
   const [fatality, setFatality] = useState(false);
   const { setTabToDelete, allTabs, setAllTabs } = useTabContext();
-  const { darkMode } = useThemeContext();
   const { soundOn } = useSoundContext();
   const { selectAll, setSelectAll, selectedTabs, addToSelectedTabs, removeFromSelectedTabs } = useSelectContext();
 
-  // TODO Add select button to toolbar. On click will select all tabs
-  // TODO If a tab is selected, delete button will replace random button
-  
-    // Reduce all tabs to one array used in selectHandler and randomClick functions
-    const tabsList = Object.values(allTabs).reduce((acc, curr): Tab[] => {
-      acc.push(...curr);
-      return acc;
-    }, []);
+
+  // Reduce all tabs to one array used in selectHandler and randomClick functions
+  const tabsList = Object.values(allTabs).reduce((acc, curr): Tab[] => {
+    acc.push(...curr);
+    return acc;
+  }, []);
 
   useEffect(() => {
     const checkbox = document.getElementById('select-all') as HTMLInputElement;
@@ -58,32 +57,41 @@ const ToolbarLeft = () => {
 
   // Selects a tab at random to delete from the tabsList array above
   const randomClick = async () => {
-      const randNum = Math.floor(Math.random() * tabsList.length);
-      const randTab = tabsList[randNum];
+    const randNum = Math.floor(Math.random() * tabsList.length);
+    const randTab = tabsList[randNum];
 
-      if (randTab) {
-        if(soundOn) setFatality(prev => !prev);
-        if (setTabToDelete) setTabToDelete(randTab.tabId!);
-        const { active } = randTab;
-        let timeToRemove: number;
-        if (active) timeToRemove = 1500;
-        else timeToRemove = 500;
-    
-        setTimeout(chrome.tabs.remove, timeToRemove, randTab.tabId);
-        setTimeout(() => getTabs().then(tabs => {
-          if (setAllTabs) setAllTabs(tabs);
-        }), timeToRemove + 100);
-        if(soundOn) setTimeout(() => setFatality(prev => !prev), 1500);
-      }
+    if (randTab) {
+      if (soundOn) setFatality(prev => !prev);
+      if (setTabToDelete) setTabToDelete(randTab.tabId!);
+      const { active } = randTab;
+      let timeToRemove: number;
+      if (active) timeToRemove = 1500;
+      else timeToRemove = 500;
+
+      setTimeout(chrome.tabs.remove, timeToRemove, randTab.tabId);
+      setTimeout(() => getTabs().then(tabs => {
+        if (setAllTabs) setAllTabs(tabs);
+      }), timeToRemove + 100);
+      if (soundOn) setTimeout(() => setFatality(prev => !prev), 1500);
+    }
   };
 
   return (
     <div className='flex items-center'>
-      <input type="checkbox" id="select-all" className='checkbox checkbox-primary mx-0.5' checked={selectAll} onChange={selectHandler} />
-      <button className={ `tooltip tooltip-right btn btn-primary btn-square btn-sm h-9 w-9 mx-1 ${!darkMode ? 'btn-outline' : ''}` } id="random" data-tip='Close a tab at random' onClick={randomClick}>
-      <img className='flex justify-center items-center w-fit h-fit p-[.175rem] stroke-current' src={darkMode ? darkRandom : lightRandom} alt="Random mode button" />
-      </button>
-      {fatality && soundOn && <Fatality play={fatality}/>}
+      <input type="checkbox" id="select-all" className='checkbox checkbox-primary mx-0.5' checked={ selectAll } onChange={ selectHandler } />
+
+      <span className={`flex items-center transition-opacity duration-300 ${selectedTabs.length ? 'hidden opacity-0': 'opacity-100'}`}>
+        <button className='tooltip tooltip-right btn btn-ghost btn-square btn-sm h-9 w-9 mx-1' id="random" data-tip='Close a tab at random' onClick={ randomClick }>
+          <img className='flex justify-center items-center w-fit h-fit p-[.175rem]' src={ random } alt="Random mode button" />
+        </button>
+      </span>
+
+      <span className={`flex items-center transition-opacity duration-300 ${selectedTabs.length ? 'opacity-100' : 'hidden opacity-0'}`}>
+        <button className='btn btn-ghost btn-square btn-sm h-9 w-9 mx-1'>
+          <img className='fill-primary stroke-primary w-6 h-6' src={trash} alt="Delete button" />
+        </button>
+      </span>
+      { fatality && soundOn && <Fatality play={ fatality } /> }
     </div>
   )
 }
