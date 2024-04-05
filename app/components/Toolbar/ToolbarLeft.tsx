b7ec3b33ea56504a19fe00b230702cc2f3256383
Sunny Pacheco <pacheco.sunny@gmail.com>
2024-04-05 14:32:04 -0400

    Add: Minimize windows button

--- app/components/Toolbar/ToolbarLeft.tsx
+++ app/components/Toolbar/ToolbarLeft.tsx
@@ -7,6 +7,7 @@ import { useSoundContext } from '../../context/SoundContext';
 import { useSelectContext } from '../../context/SelectContext';
 import { useFontContext } from '../../context/FontContext';
 import { Tab } from '../../../types';
+import windowMin from '../../icons/windowMin.svg';
 import random from '../../icons/random.svg';
 import trash from '../../icons/trash.svg';
 
@@ -17,7 +18,7 @@ const ToolbarLeft = () => {
   const { smallActive } = useFontContext();
   const { soundOn } = useSoundContext();
   const { selectAll, setSelectAll, selectedTabs, addToSelectedTabs, removeFromSelectedTabs } = useSelectContext();
-  
+
   const checkboxRef = useRef<HTMLInputElement>(null);
   const resolvedRef = checkboxRef.current as HTMLInputElement;
 
@@ -57,8 +58,8 @@ const ToolbarLeft = () => {
     }
   }
 
-  // Selects a tab at random to delete from the tabsList array above
-  const randomClick = async () => {
+  // Selects a tab at random to close from the tabsList array above
+  const randomClick = () => {
     const randNum = Math.floor(Math.random() * tabsList.length);
     const randTab = tabsList[randNum];
 
@@ -74,6 +75,7 @@ const ToolbarLeft = () => {
     }
   };
 
+  // Close selected tabs
   const removeTabs = () => {
     let activeTab = false;
     for (let tab of selectedTabs) {
@@ -102,21 +104,35 @@ const ToolbarLeft = () => {
     setTimeout(updateTabs, timeout + 50);
   };
 
+  // Minimize windows
+  const minimizeWindows = () => {
+    for (const window in allTabs) {
+      chrome.windows.update(Number(window), { state: 'minimized' });
+    };
+  };
+
   return (
     <div className='flex items-center'>
-      <input type="checkbox" id="select-all" ref={checkboxRef} className={ `checkbox checkbox-primary mx-0.5 ${smallActive ? '' : 'checkbox-lg'}` } checked={ selectAll } onChange={ selectHandler } />
+      <input type="checkbox" id="select-all" ref={ checkboxRef } className={ `tooltip tooltip-top checkbox checkbox-primary mx-0.5 ${smallActive ? '' : 'checkbox-lg'}` } checked={ selectAll } onChange={ selectHandler } data-tip='Select All' />
 
       {/* Random Button */ }
-      <span className={ `tooltip tooltip-right flex items-center transition-opacity duration-300 ${selectedTabs.length ? 'hidden opacity-0' : 'opacity-100'}` } data-tip='Close a tab at random'>
-        <ToolbarButton clickFunc={ randomClick } smallActive={smallActive}>
+      <span className={ `tooltip tooltip-top flex items-center justify-center transition-opacity duration-300 w-fit ${selectedTabs.length ? 'hidden opacity-0' : 'opacity-100'}` } data-tip='Close a tab at random'>
+        <ToolbarButton clickFunc={ randomClick } smallActive={ smallActive }>
           <img className={ `flex justify-center items-center p-[.175rem] ${smallActive ? 'w-[34px] h-[34px]' : 'w-[42px] h-[42px'}` } src={ random } alt="Random mode button" />
         </ToolbarButton>
       </span>
 
       {/* Delete Button */ }
-      <span className={ `flex items-center transition-opacity duration-300 ${selectedTabs.length ? 'opacity-100' : 'hidden opacity-0'}` }>
-        <ToolbarButton clickFunc={ removeTabs } smallActive={smallActive}>
-          <img className={ `flex justify-center items-center ${smallActive ? 'w-6 h-6' : 'w-8 h-8'}` } src={ trash } alt="Delete button" />
+      <span className={ `flex items-center justify-center transition-opacity duration-300 w-fit ${selectedTabs.length ? 'opacity-100' : 'hidden opacity-0'}` }>
+        <ToolbarButton clickFunc={ removeTabs } smallActive={ smallActive }>
+          <img className={ `flex justify-center items-center ${smallActive ? 'w-6 h-6' : 'w-8 h-8'}` } src={ trash } alt="Close tabs button" />
+        </ToolbarButton>
+      </span>
+
+      {/* Minimize Button */ }
+      <span className='tooltip tooltip-top flex items-center justify-center w-fit' data-tip='Minimize all windows'>
+        <ToolbarButton clickFunc={ minimizeWindows } smallActive={ smallActive }>
+          <img className={ `flex justify-center items-center ${smallActive ? ' w-[32px] h-[32px]' : 'w-10 h-10'}` } src={ windowMin } alt="Minimize windows button" />
         </ToolbarButton>
       </span>
 
